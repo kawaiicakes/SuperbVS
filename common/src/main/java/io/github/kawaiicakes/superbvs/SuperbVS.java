783b725aa51e814b7b00a6d37ab91b05edffda8c
kawaiicakes <vomitmopper@gmail.com>
2025-06-16 11:23:24 -0600

    Using Gradle configuration of SBW's main branch as previous Arch setup was broken

--- common/src/main/java/io/github/kawaiicakes/superbvs/SuperbVS.java
+++
@@ -1,8 +0,0 @@
-package io.github.kawaiicakes.superbvs;
-
-public final class SuperbVS {
-    public static final String MOD_ID = "superbvs";
-
-    public static void init() {
-    }
-}
---
+++ src/main/java/io/github/kawaiicakes/superbvs/SuperbVS.java
@@ -0,0 +1,15 @@
+package io.github.kawaiicakes.superbvs;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(SuperbVS.MOD_ID)
+public class SuperbVS {
+    public static final String MOD_ID = "superbvs";
+    public static final Logger LOGGER = LogManager.getLogger(SuperbVS.class);
+
+    public SuperbVS() {
+
+    }
+}
